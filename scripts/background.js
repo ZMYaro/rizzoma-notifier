b8170c68c0e5d9a333be74fa91265ebe817f0f77
Zachary Yaro <zmyaro@gmail.com>
2013-10-19 23:16:35 -0400

    Initial commit with desktop notifications of new waves

---
+++ scripts/background.js
@@ -0,0 +1,107 @@
+
+
+function makeIFrame() {
+	// Remove existing iframes.
+	var iframes = document.getElementsByTagName('iframe');
+	while(iframes.length > 0) {
+		iframes[0].parentElement.removeChild(iframes[0]);
+	}
+	
+	var iframe = document.createElement('iframe');
+	iframe.src = 'https://rizzoma.com/topic/';
+	iframe.id = 'rizNotifIFrame';
+	iframe.name = 'rizNotifIFrame';
+	document.body.appendChild(iframe);
+}
+
+
+function notifUnreadWaves(waves) {
+	console.log('Checking which waves are unread.');
+	
+	// Create arrays for the unread wave objects and the notification items.
+	var unreadWaves = [];
+	var notifItems = [];
+	// For each wave object...
+	for(var i = 0; i < waves.length; i++) {
+		// If this wave is unread,
+		if(waves[i].totalUnreadBlipCount > 0) {
+			// Add it to the array of unread waves,
+			unreadWaves.push(waves[i]);
+			// And add its title and snippet to the array of notification items.
+			notifItems.push({
+				title: waves[i].title,
+				message: waves[i].snippet
+			});
+		}
+	}
+	
+	console.log(notifItems.length + ' waves are unread.');
+	
+	if(notifItems.length === 1) {
+		// If there is only one unread wave, display its data as a basic notification.
+		chrome.notifications.create(unreadWaves[0].waveId, {
+			type: 'basic',
+			title: unreadWaves[0].title,
+			message: unreadWaves[0].snippet,
+			iconUrl: chrome.extension.getURL('/images/riz_icon_128.png')
+		}, function(notifId) {
+			if(chrome.runtime.lastError) {
+				console.error('Error creating notification \"' + notifId + '\": ');
+				console.error(chrome.runtime.lastError);
+			} else {
+				console.log('Notification \"' + notifId + '\" successfully created.');
+			}
+		});
+	} else if(notifItems.length > 1) {
+		// If there are multiple unread waves, display their data as a list notification.
+		chrome.notifications.create('multi', {
+			type: 'list',
+			title: notifItems.length + ' new messages',
+			message: '',
+			iconUrl: chrome.extension.getURL('/images/riz_icon_128.png'),
+			items: notifItems
+		}, function(notifId) {
+			if(chrome.runtime.lastError) {
+				console.error('Error creating notification \"' + notifId + '\": ');
+				console.error(chrome.runtime.lastError);
+			} else {
+				console.log('Notification \"' + notifId + '\" successfully created.');
+			}
+		});
+	}
+}
+
+chrome.alarms.onAlarm.addListener(function(alarm) {
+	if(alarm.name === REFRESH_ALARM_NAME) {
+		console.log('Refresh alarm fired.');
+		fetchNewUnreadWaves(notifUnreadWaves);
+	}
+});
+chrome.notifications.onClicked.addListener(function(notificationId) {
+	if(notificationId === 'multi') {
+		loadRizTab('');
+	} else {
+		loadRizTab(notificationId);
+	}
+});
+
+window.addEventListener('load', function() {
+	console.log('Extension started.');
+	makeIFrame();
+	fetchNewUnreadWaves(notifUnreadWaves);
+	updateAlarm();
+}, false);
+
+/*chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+	if(message === "GET_ACCESS_TOKEN") {
+		chrome.tabs.query({
+			url: "*://rizzoma.com/topic/*"
+		}, function(tabs) {
+			if(tabs.length > 0) {
+				chrome.tabs.sendMessage(tabs[0].id, "GET_ACCESS_TOKEN", function(response) {
+					sendResponse(response);
+				});
+			}
+		});
+	}
+});*/
---
+++ scripts/shared.js
@@ -0,0 +1,100 @@
+var RIZ_API_URL = "https://rizzoma.com/api/rest/1/wave/searchBlipContent/?queryString=&ptagNames=FOLLOW";
+var REFRESH_ALARM_NAME = "refresh";
+var defaults = {
+	refreshTime: 1
+};
+
+function updateAlarm(mins) {
+	chrome.storage.local.get({
+		refreshTime: defaults.refreshTime
+	}, function(items) {
+		chrome.alarms.create(REFRESH_ALARM_NAME, {
+			periodInMinutes: items.refreshTime
+		});
+		
+		chrome.notifications.create('alarmset', {
+			type: 'basic',
+			title: 'Test',
+			message: 'Alarm created.',
+			iconUrl: chrome.extensions.getURL('/images/riz_icon_128.png')
+		}, function(notifId) {
+			if(chrome.runtime.lastError) {
+				console.error('Error creating notification \"' + notifId + '\": ');
+				console.error(chrome.runtime.lastError);
+			} else {
+				console.log('Notification \"' + notifId + '\" successfully created.');
+			}
+		});
+	});
+}
+/**
+ * Fetch waves that have been updated since the last refresh and pass them to the callback function
+ * @param {Function} successCallback - The function to which the wave data should be sent
+ */
+function fetchNewUnreadWaves(successCallback) {
+	console.log('Preparing to fetch new unread waves.');
+	chrome.storage.local.get('lastRefreshTime', function(items) {
+		var lastRefreshTime = items.lastRefreshTime || 0;
+		chrome.storage.local.set({'lastRefreshTime': Math.floor(Date.now() / 1000)}, function() {});
+		fetchUnreadWaves(lastRefreshTime, successCallback);
+	});
+}
+/**
+ * Fetch unread waves and pass them to the callback function
+ * @param {Number} lastSearchDate - If specified, only waves changed since this timestamp will be returned
+ * @param {Function} successCallback - The function to which the wave data should be sent
+ */
+function fetchUnreadWaves(lastSearchDate, successCallback) {
+	console.log('Preparing to fetch waves changed since ' + lastSearchDate + '.');
+	// If lastSearchDate has no value, set it to zero (fetch all waves).
+	if(lastSearchDate === null || lastSearchDate === undefined) {
+		lastSearchDate = 0;
+	}
+	// Fetch the saved expressSession id.
+	chrome.storage.local.get("expressSessionId", function(items) {
+		if(!items.expressSessionId) {
+			makeIFrame();
+		} else {
+			var apiURL = RIZ_API_URL + '&lastSearchDate=' + lastSearchDate +
+				'&ACCESS_TOKEN=' + encodeURIComponent(items.expressSessionId);
+			var xhr = new XMLHttpRequest();
+			xhr.open("GET", apiURL, true);
+			xhr.onreadystatechange = function() {
+				if(xhr.readyState === 4) {
+					if(xhr.status === 200) {
+						var response = JSON.parse(xhr.responseText);
+						if(response.data && response.data.searchResults) {
+							successCallback(response.data.searchResults);
+						} else {
+							// TODO: dispError();
+						}
+					}
+				}
+			};
+			xhr.send();
+		}
+	});
+}
+
+function loadRizTab(waveId) {
+	chrome.tabs.query({
+		url: '*://*.rizzoma.com/topic/*'
+	}, function(tabs) {
+		// If a Rizzoma tab exists, update its URL and bring it to the front.
+		if(tabs.length > 0) {
+			chrome.tabs.update(tabs[0].id, {
+				url: 'https://rizzoma.com/topic/' + waveId,
+				active: true
+			});
+			chrome.windows.update(tabs[0].windowId, {
+				focused: true
+			});
+		// If no Rizzoma tab exists, create one.
+		} else {
+			chrome.tabs.create({
+				url: 'https://rizzoma.com/topic/' + waveId,
+				active: true
+			});
+		}
+	});
+}
