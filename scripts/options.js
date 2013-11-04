1f598119e5985a5cf8061903c6e91eead3ee9519
Zachary Yaro <zmyaro@gmail.com>
2013-11-04 10:17:19 -0500

    Add options page (no options available yet)

---
+++ scripts/options.js
@@ -0,0 +1,24 @@
+
+
+function init() {
+	setUpChromeLinks();
+}
+function setUpChromeLinks() {
+	// Get the list of <a>s.
+	var links = document.getElementsByTagName("a");
+	// For each link,
+	for(var i = 0; i < links.length; i++) {
+		// if the URL begins with "chrome://",
+		if(links[i].href.indexOf("chrome://") === 0) {
+			// tell it to goToPage onclick.
+			links[i].onclick = goToPage;
+		}
+	}
+}
+function goToPage(e) {
+	// Prevent the browser from following the link.
+	e.preventDefault();
+	chrome.tabs.update({ url: e.target.href });
+}
+
+window.addEventListener("load", init, false);
