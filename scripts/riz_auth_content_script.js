8831d1c6214f8e3a46e2985075136dd011cacc5c
Zachary Yaro <zmyaro@gmail.com>
2014-08-03 17:12:00 -0400

    Wrap the content script in a closure

--- scripts/riz_auth_content_script.js
+++ scripts/riz_auth_content_script.js
@@ -1,27 +1,29 @@
-if(window.name !== 'rizNotifIFrame') {
-	return;
-}
-
-var addAuthDiv = function() {
-	if(!document.getElementById('expressSessionId')) {
-		var tokenElem = document.createElement('div');
-		tokenElem.id = 'expressSessionId';
-		tokenElem.innerHTML = (window.expressSession ? window.expressSession.id : 'undefined');
-		document.body.appendChild(tokenElem);
+(function() {
+	if(window.name !== 'rizNotifIFrame') {
+		return;
 	}
-};
 
-var pgScript = document.createElement('script');
-pgScript.type = 'text/javascript';
-pgScript.innerText = pgScript.textContent = '(' + addAuthDiv + ')();';
-(document.head || document.documentElement).appendChild(pgScript);
+	var addAuthDiv = function() {
+		if(!document.getElementById('expressSessionId')) {
+			var tokenElem = document.createElement('div');
+			tokenElem.id = 'expressSessionId';
+			tokenElem.innerHTML = (window.expressSession ? window.expressSession.id : 'undefined');
+			document.body.appendChild(tokenElem);
+		}
+	};
 
-chrome.storage.local.set({
-	expressSessionId: document.getElementById('expressSessionId').innerText
-}, function() {});
+	var pgScript = document.createElement('script');
+	pgScript.type = 'text/javascript';
+	pgScript.innerText = pgScript.textContent = '(' + addAuthDiv + ')();';
+	(document.head || document.documentElement).appendChild(pgScript);
 
-/*chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-	if(message === 'GET_ACCESS_TOKEN') {
-		sendResponse(document.getElementById('expressSessionId').dataset.token);
-	}
-});*/
+	chrome.storage.local.set({
+		expressSessionId: document.getElementById('expressSessionId').innerText
+	}, function() {});
+
+	/*chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+		if(message === 'GET_ACCESS_TOKEN') {
+			sendResponse(document.getElementById('expressSessionId').dataset.token);
+		}
+	});*/
+})();
